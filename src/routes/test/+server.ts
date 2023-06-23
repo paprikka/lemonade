dbcacfe208f8e906f5e8f4d8fd55cdb1087cf771
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-06-23 19:14:43 +0100

    wip:

--- src/routes/test/+server.ts
+++ src/routes/test/+server.ts
@@ -1,15 +1,22 @@
 import fs from 'fs';
 import path from 'path';
-export const GET = async ({ request }) => {
-	// read all files in the current module dir using node fs module:
 
-	const files = fs.readdirSync(path.resolve('./src/routes/test'));
+export const GET = async ({}) => {
+	// get the current file path:
+	const __filename = path.resolve(import.meta.url);
+
+	// read all files in the folder of __filename using node fs module:
+
+	const filesHere = fs.readdirSync(path.dirname(__filename));
+
+	// const files = fs.readdirSync(path.resolve('./src/routes/test'));
 
 	// get current absolute path:
 	const __dirname = path.resolve();
 
 	const data = {
-		files,
+		filesHere,
+		__filename,
 		__dirname
 	};
 
