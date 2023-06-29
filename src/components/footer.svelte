b294edb85612916d76a759d3cfab41871560030f
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-06-29 13:36:11 +0100

    feat: make the footer smaller

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -3,12 +3,11 @@
 		Made by 🐐 and <a href="https://sonnet.io">Rafal Pastuszak</a>. Come and
 		<a href="https://sonnet.io/posts/hi">say hi</a>.
 	</p>
-	<p class="small">No cookies. My dog ate the cookies 🌻</p>
 </footer>
 
 <style>
 	footer {
-		padding: 0.5rem 0 1.5rem;
+		padding: 0.5rem 0;
 		font-size: var(--font-size-xs);
 		text-align: center;
 		border-top: 0.1rem solid var(--color-text);
@@ -21,10 +20,5 @@
 
 	p {
 		margin: 0;
-	}
-
-	.small {
-		font-size: var(--font-size-xxs);
-		opacity: 0.8;
 	}
 </style>
